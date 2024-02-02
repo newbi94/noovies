11eae61268155dc9e2cf64264e8fc3c789cbb02e
newbi94 <newbi94@gmail.com>
2024-02-02 17:07:44 +0900

    24.02.02
    React-Native Master Class  Start
    환경구성 세팅,
    안드로이드 에뮬레이터 기종 호환(?)문제,
    SplashScreen (not AppLoading),
    useCallback,
    onLayout,

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from 'react-native';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+
+
+export default function App () {
+  const [ready, setReady] = useState(false);
+  
+  
+  useEffect( async () => {
+    async function prepare() {
+      try {
+        await new Promise((resolve)=> setTimeout(resolve, 8000))
+        // pre-load fonts, call APIs, etc
+        // 강의의 startLoading과 동일하게 동작
+      } catch (error) {
+        // 강의의 onError와 동일하게 동작
+        console.warn(error);
+      } finally {
+        // 강의의 onFinish와 동일하게 동작
+        setReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) await SplashScreen.hideAsync();
+  }, [ready]); //Hide the native splash screen immediately, ready가 true가 되면
+              //splash screen을 숨긴다.
+              //useCallback -> 함수는 랜더링될 때 늘 새로운 참조값을 갖지만
+              //위 함수를 사용하면 값이 실제로 바뀔 때만 새로운 참조값이 된다.
+  if (!ready) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView}>
+      <Text>We are done Loading !</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
