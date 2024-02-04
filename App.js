0ee74db8fa2b957cedeec9236508814447517ecd
newbi94 <newbi94@gmail.com>
2024-02-04 19:34:51 +0900

    24.02.04
    BottomTabNavigatior,
    onLayout & onReady,
    useEffect & async , await,
    유난히 에러가 많았던 날
    노션 에러노트가 오랜만에 든든하게 채워졌다.

--- App.js
+++ App.js
@@ -2,21 +2,18 @@ import React, { useCallback, useEffect, useState } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { Text, View, Image } from 'react-native';
 import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import { Asset, useAssets, } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
+
 SplashScreen.preventAutoHideAsync();
 
 
 export default function App () {
   const [ready, setReady] = useState(false);
 
-  /* const [assets] = useAssets([require("./1.jpg")])
-  const [loaded] = Font.useFonts(Ionicons.font) */
-  //아래 장황한 코드들을 단 두줄로 줄일 수 있는 Hook이다.
-  //useAssets과 useFonts를 쓰면 간단하게 preload할 수 있다.
- //단 Image.prefetch 처럼 외부에서 파일을 preload할 수는 없다.
   const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-  
   const loadImages = (images) =>
     images.map((image) => {
       if (typeof image === "string") {
@@ -24,39 +21,49 @@ export default function App () {
       } else {
         return Asset.loadAsync(image);
       }
-    });//외부 서버로부터 파일을 preload할 때는 Image.prefetch를,
-       //noovies폴더 내부에 있는 파일을 preload할 때는 Asset.loadAsync를 한다.
-  
-  useEffect( async () => {
-    async function prepare() {
-      try {
-        const fonts = loadFonts([Ionicons.font]);
-        const images = loadImages([require("./1.jpg"),"https://www.dong-wha.co.kr/product/pimage/324_img21.jpg"])
-        await Promise.all([...fonts, ...images]);
-        //fonts와 images의 promise들에 await을 붙여주는 Promise.all
-      } catch (error) {
-        console.warn(error);
-      } finally {
-        setReady(true);
+    });
+    //Error: java.security.cert.CertPathValidatorException: Trust anchor for certification path not found
+    //외부 url로부터 파일을 받아올 때 인증서에 관한 에러다.
+    //image 부분에서 뒤에 있는 url로부터 파일을 받아오는 부분을 그냥 삭제해서 해결했다.
+
+       async function prepare() {
+          try {
+            const fonts = loadFonts([AntDesign.font]);
+            const images = loadImages([require("./1.png")])
+            await Promise.all([...fonts, ...images]);
+          } catch (error) {
+            console.warn(error);
+          } finally {
+            setReady(true);
+            console.log("setReady")
+          }
+        }
+
+       useEffect(() => {
+        prepare();
+      }, []);
+      //useEffect내에 prepare 함수를 넣어놨었는데, 자꾸 문제가 발생해서 바깥으로 빼서 해결했다.
+      //async를 useEffect 내부에서 붙여줄거면 함수로 묶어서 붙여주래서 그렇게 했는데도 잘 해결되지 않았었다.
+      //useEffect에 대한 공부가 더 필요하다.
+      
+      const onLayoutRootView = useCallback(async () => {
+        if (ready) {
+          await SplashScreen.hideAsync();
+          console.log("READY to hide")
+        }
+      }, [ready]);
+    
+      if (!ready) {
+        return null;
       }
-    }
+      return (
+        
+        <NavigationContainer onReady={onLayoutRootView}>
+      <Tabs />
+    </NavigationContainer>
+    
+      );
+    }//onReady는 네비게이션 한정 prop인듯 하다.
+    //기존 onLayout을 유지하면 아무런 에러도 없이 splashscreen에서 영겁의 시간을 보내야한다.
 
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (ready) await SplashScreen.hideAsync();
-  }, [ready]); //Hide the native splash screen immediately, ready가 true가 되면
-              //splash screen을 숨긴다.
-              //useCallback -> 함수는 랜더링될 때 늘 새로운 참조값을 갖지만
-              //위 함수를 사용하면 값이 실제로 바뀔 때만 새로운 참조값이 된다.
-  if (!ready) {
-    return null;
-  }
-
-  return (
-    <View onLayout={onLayoutRootView}>
-      <Text>We are done Loading !</Text>
-    </View>
-  );
-}
+    
