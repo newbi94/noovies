cc8be4377c86050f9a01140eb249f93714b718fc
newbi94 <newbi94@gmail.com>
2024-02-09 19:59:04 +0900

    24.02.09
    contentContainerStyle & style
    about <ScrollView>,
    color opacity,
    refactoring,
    integrate 3 different fetch about useEffect ( for Loading),

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,139 +1,192 @@
-import React, { useState, useEffect } from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   Dimensions,
-  StyleSheet,
-  useColorScheme,
-  Appearance,
+  ScrollView,
 } from "react-native";
+import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import Swiper from 'react-native-web-swiper';
-import { makeImgPath } from "../utils";
-import { BlurView } from 'expo-blur';
+import Slide from "../components/Slide";
+import Poster from "../components/Poster";
 
 const Container = styled.ScrollView`
-  //background-color: ${(props) => props.theme.mainBgColor};
-`;//Tab Navigator자체에 sceneContainerStyle로 배경색을 설정했기 때문에 생략할 수 있다.
-
-const View = styled.View`
-  flex:1;
 `;
 
 const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  //background-color: ${(props) => props.theme.mainBgColor};
-`;//Tab Navigator자체에 sceneContainerStyle로 배경색을 설정했기 때문에 생략할 수 있다.
+`;
 
-const BgImg = styled.Image`
-`;
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-const Title = styled.Text<{ isDark: boolean }>`
-  font-size: 16px;
+const ListTitle = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 18px;
   font-weight: 600;
-  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+  margin-left: 30px;
 `;
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  width: 90%;
-  margin: 0 auto;
-  justify-content: space-around;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
   align-items: center;
 `;
-const Column = styled.View`
-  width: 60%;
-`;
-const Overview = styled.Text<{ isDark: boolean }>`
-  margin-top: 10px;
-  color: ${(props) =>
-    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
-`;
-const Votes = styled(Overview)`
-  font-size: 12px;
-`;//styled( x ) => x의 styled 속성을 가져온다.
 
-const { height : SCREEN_HEIGHT } = Dimensions.get("window");
+const Title = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+`;
+
+const Votes = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 10px;
+`;
+
+const ListContainer = styled.View`
+  margin-bottom: 40px;
+`;
+
+const HMovie = styled.View`
+  padding: 0px 30px;
+  margin-bottom: 30px;
+  flex-direction: row;
+`;
+
+const HColumn = styled.View`
+  margin-left: 15px;
+  width: 80%;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  opacity: 0.8;
+  width: 80%;
+`;
+
+const Release = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 12px;
+  margin-right: 10px;
+`;
+const ComingSoonTitle = styled(ListTitle)`
+  margin-bottom: 30px;
+`;
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const API_KEY = '1ca048192caae0c193269bc2b692dabc'
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState<any[]>([]);
+  const [upcoming, setUpcoming] = useState<any[]>([]);
+  const [trending, setTrending] = useState<any[]>([]);
   
-  const isDark = useColorScheme() === "dark";
-
-  const getNowPlaying = async () => {
-    
-    const API_KEY = '1ca048192caae0c193269bc2b692dabc'
-     
+  const getTrending = async () => {
     const { results } = await (
       await fetch(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
       )
     ).json();
+    setTrending(results);
+  };
+  
+  const getUpcoming = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setUpcoming(results);
+  };
+  
+  const getNowPlaying = async () => {
+     
+  const { results } = await (
+    await fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+    )
+  ).json();
     setNowPlaying(results);
+  };
+  const getData = async () => {
+    await Promise.all([getTrending(), getUpcoming(), 
+      getNowPlaying()]);
     setLoading(false);
-  };  
-  // const { testOne, testTwo } = x
-  // => const testOne = x.testOne; const testTwo = x.testTwo;
-  /*const a = {
-    b: "b",
-    c: "c",
-  }
-  const z = a.b
-  console.log(z)
-  */
+  };
+  
+  console.log("this type is", typeof nowPlaying)
+  console.log("is this array?" , Array.isArray(nowPlaying))
+  
   useEffect(() => {
-      
-      getNowPlaying();
+      getData();
     }, []);
-  console.log(nowPlaying)
 
-    return loading ? ( 
-  <Loader>  
-    <ActivityIndicator/>
-  </Loader> 
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
   ) : (
-  <Container>
-    <Swiper 
-    loop
-    timeout={4}
-    controlsEnabled={false}
-    containerStyle={{width: "100%", height: SCREEN_HEIGHT / 4}}>
-      {nowPlaying.map(movie => 
-          <View key={movie.id}>
-            <BgImg
-              style={StyleSheet.absoluteFill}
-              source={{ uri: makeImgPath(movie.backdrop_path) }}
-            />
-             <BlurView
-              tint={isDark ? "dark" : "light"}
-              intensity={85}
-              style={StyleSheet.absoluteFill}
-            >
-              <Wrapper>
-                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
-                <Column>
-                  <Title isDark={isDark}>{movie.original_title}</Title>
-                  {movie.vote_average > 0 ? (
-                    <Votes isDark={isDark}>⭐️ {movie.vote_average}/10</Votes>
-                  ) : null}
-                  <Overview isDark={isDark}>
-                    {movie.overview.slice(0, 100)}...
-                  </Overview>
-                </Column>
-              </Wrapper>
-            </BlurView>
-          </View>
-        )}
-    </Swiper>
-  </Container>
-  )
+    <Container>
+      <Swiper 
+        loop
+        timeout={4}
+        controlsEnabled={false}
+        containerStyle={{
+          marginBottom: 40,
+          width: "100%",
+          height: SCREEN_HEIGHT / 4,
+        }}>
+        {nowPlaying.map((movie) => (
+          <Slide 
+          key={movie.id}
+          backdropPath={movie.backdrop_path}
+          posterPath={movie.poster_path}
+          originalTitle={movie.original_title}
+          voteAverage={movie.vote_average}
+          overview={movie.overview}
+          />))}
+        </Swiper>
+        <ListTitle>Trending Movies</ListTitle>
+      <TrendingScroll
+        contentContainerStyle={{ paddingLeft: 30 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {trending.map((movie) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path} />
+            <Title>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 ? "..." : null}
+            </Title>
+            {movie.vote_average === 0 ? null : (
+            <Votes>
+              ⭐️ {movie.vote_average}/10
+              </Votes>)}
+          </Movie>
+        ))}
+      </TrendingScroll>
+      <ListContainer>
+        <ComingSoonTitle>Comming Soon!</ComingSoonTitle>
+        {upcoming.map((movie) => (
+        <HMovie key={movie.id}>
+          <Poster path={movie.poster_path} />
+          <HColumn>
+          <Title>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 ? "..." : null}
+            </Title>
+            <Release>comming soon</Release>
+            <Overview>{movie.overview}</Overview>
+            </HColumn>
+        </HMovie>))}
+      </ListContainer>
+    </Container>
+  );
 };
-
 export default Movies;
