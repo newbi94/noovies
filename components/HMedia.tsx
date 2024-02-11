48a62c54d413906c3578f227a872a103a87b9a0a
newbi94 <newbi94@gmail.com>
2024-02-11 17:27:54 +0900

    24.02.11
    Refactoring(VMedia,HMedia,Votes),
    FlatList,

---
+++ components/HMedia.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "styled-components/native";
+import Poster from "./Poster";
+import Votes from "./Votes";
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-weight: 600;
+  margin-top: 7px;
+`;
+
+const HMovie = styled.View`
+  padding: 0px 30px;
+  margin-bottom: 30px;
+  flex-direction: row;
+`;
+
+const HColumn = styled.View`
+  margin-left: 15px;
+  width: 80%;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  opacity: 0.8;
+  width: 80%;
+`;
+
+const Release = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 12px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-weight: 500;
+  opacity: 0.6;
+`;
+
+interface HMediaProps {
+    posterPath: string;
+    originalTitle: string;
+    overview: string;
+    releaseDate?: string;
+    voteAverage?: number;
+}
+/* ?를 붙여 선택적으로 props를 받지만, 아래에 해당 코드처럼 
+?를 통해 받을 때와 받지 않을 때를 설정하지 않으면 앱 가동에는 큰 문제가
+없지만 에러가 뜬다. 
+{new Date(releaseDate).toLocaleDateString("ko", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    })} */
+
+const HMedia:React.FC<HMediaProps> = ({
+    posterPath,
+    originalTitle,
+    releaseDate,
+    overview,
+    voteAverage,
+    }) => {
+        return(
+        <HMovie>
+            <Poster path={posterPath} />
+            <HColumn>
+                <Title>
+                    {originalTitle.length > 30
+                        ? `${originalTitle.slice(0, 30)}...`
+                        : originalTitle}
+                </Title>
+                {releaseDate ? (
+                    <Release>
+                        {new Date(releaseDate).toLocaleDateString("ko", {
+                        month: "long",
+                        day: "numeric",
+                        year: "numeric",
+                        })}
+                    </Release>
+                ) : null}
+                    {voteAverage ? <Votes votes={voteAverage} /> : null}
+                <Overview>
+                    {overview !== "" && overview.length > 140
+                    ? `${overview.slice(0, 140)}...`
+                    : overview}
+                </Overview>
+            </HColumn>
+        </HMovie>
+        )
+}
+
+export default HMedia;
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -3,13 +3,16 @@ import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   Dimensions,
+  FlatList,
   RefreshControl,
   ScrollView,
+  View,
 } from "react-native";
 import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
 import Slide from "../components/Slide";
-import Poster from "../components/Poster";
+import VMedia from "../components/VMedia";
+import HMedia from "../components/HMedia";
 
 const Container = styled.ScrollView`
 `;
@@ -27,56 +30,18 @@ const ListTitle = styled.Text`
   margin-left: 30px;
 `;
 
-const TrendingScroll = styled.ScrollView`
+/* const TrendingScroll = styled.FlatList`
   margin-top: 20px;
-`;
-
-const Movie = styled.View`
-  margin-right: 20px;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  font-weight: 600;
-  margin-top: 7px;
-  margin-bottom: 5px;
-`;
-
-const Votes = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  font-size: 10px;
-`;
+`; */
 
 const ListContainer = styled.View`
   margin-bottom: 40px;
 `;
 
-const HMovie = styled.View`
-  padding: 0px 30px;
-  margin-bottom: 30px;
-  flex-direction: row;
-`;
-
-const HColumn = styled.View`
-  margin-left: 15px;
-  width: 80%;
-`;
-
-const Overview = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  opacity: 0.8;
-  width: 80%;
-`;
-
-const Release = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  font-size: 12px;
-  margin-right: 10px;
-`;
 const ComingSoonTitle = styled(ListTitle)`
-  margin-bottom: 30px;
+  margin-bottom: 20px;
 `;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const API_KEY = '1ca048192caae0c193269bc2b692dabc'
@@ -130,25 +95,23 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   useEffect(() => {
       getData();
     }, []);
+
     const onRefresh = async () => {
       setRefreshing(true);
       await getData();
       setRefreshing(false);
     }
-    //refreshControl을 만들 때 태그로 RefreshControl을 만들어주고,
-    //내부에 props를 보내줘야 한다.
+
   return loading ? (
     <Loader>
       <ActivityIndicator />
     </Loader>
   ) : (
-    <Container
-     refreshControl={
-      <RefreshControl 
+    <FlatList 
         refreshing={refreshing} 
         onRefresh={onRefresh}
-      />}
-    >
+        ListHeaderComponent={
+        <>
       <Swiper 
         loop
         timeout={4}
@@ -170,48 +133,50 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
       </Swiper>
       <ListContainer>
         <ListTitle>Trending Movies</ListTitle>
-        <TrendingScroll
-          contentContainerStyle={{ paddingLeft: 30 }}
+        <FlatList
+          style={{marginTop:20}}
+          data={trending}
           horizontal
+          keyExtractor={(item) => item.id +""}
           showsHorizontalScrollIndicator={false}
-        >
-          {trending.map((movie) => (
-            <Movie key={movie.id}>
-            <Poster path={movie.poster_path} />
-            <Title>
-              {movie.original_title.slice(0, 13)}
-              {movie.original_title.length > 13 ? "..." : null}
-            </Title>
-              {movie.vote_average === 0 ? null : (
-            <Votes>
-              ⭐️ {movie.vote_average}/10
-            </Votes>
-              )}
-            </Movie>
-          ))}
-        </TrendingScroll>
+          contentContainerStyle={{ paddingHorizontal: 30 }}
+          ItemSeparatorComponent={() => <View style={{ width: 30 }} />}
+          renderItem={({ item }) => (
+            <VMedia
+              posterPath={item.poster_path}
+              originalTitle={item.original_title}
+              voteAverage={item.vote_average}
+            />)}//FlatList는 data와 renderItem을 필수로 기입한다.
+            //FlatList가 기존의 스크롤뷰보다 뛰어난 점은 모든 elements를 한번에
+            //render하는 스크롤뷰와 다르게 화면에 나타나기 바로 직전에 render하여
+            //소위 말하는 렉을 줄여준다.
+            //keyExtractor로 각 key를 부여해주는데 +""는 숫자로 된 해당id를 받아
+            //string형태로 바꿔주는 역할을 한다. 니꼬는 string으로 받아야하는 에러가 떠서
+            //이렇게 했지만 지금은 개선이 된건지 저 부분을 빼도 에러가 나지 않는다.
+            //ItemSeparatorComponent가 바로 스크롤뷰에서 갖지 못해 VMedia에서 부여했던
+            //margin속성이다. 더 특별한 것은 공간을 주는 방식이 사이에 컴포넌트를 넣어주는 방식이라
+            //텍스트나 이미지등 원하는 것을 집어 넣을 수 있고, 똑똑하게도 마지막 VMedia컴포넌트 뒤에는
+            //추가하지 않아 따로 -1해주는 코딩을 짤 필요가 없다.
+            //<TrendingScroll> 자체를 <FlatList>로 바꿔야 하는 줄 알았는데 위에 있는 
+            //TrendingScroll의 스타일 컴포넌트를 FlatList로 바꿔주면 된다. <- 니꼬는 됐는데 나는 안된다.
+            //해서 위에 스타일 컴포넌트 지우고 태그를 <FlatList>로 바꾼 뒤 style={{marginTop:20}}으로 추가했다.
+          />  
       </ListContainer>
       <ComingSoonTitle>Comming Soon!</ComingSoonTitle>
-        {upcoming.map((movie) => (
-        <HMovie key={movie.id}>
-          <Poster path={movie.poster_path} />
-          <HColumn>
-            <Title>{movie.original_title}</Title>
-            <Release>
-              {new Date(movie.release_date).toLocaleDateString("ko", {
-                month: "long",
-                day: "numeric",
-                year: "numeric",
-              })}
-            </Release>
-            <Overview>
-                {movie.overview !== "" && movie.overview.length > 80
-                  ? `${movie.overview.slice(0, 140)}...`
-                  : movie.overview}
-            </Overview>
-          </HColumn>
-        </HMovie>))}
-    </Container>
+      </>
+      }
+      keyExtractor={(item) => item.id +""}
+      data={upcoming}
+      renderItem={({item}) => (
+          <HMedia 
+          key={item.id}
+          posterPath={item.poster_path}
+          originalTitle={item.original_title}
+          releaseDate={item.release_date}
+          overview={item.overview}
+          voteAverage={item.vote_average}
+          />)}
+        />
   );
 };
 export default Movies;
