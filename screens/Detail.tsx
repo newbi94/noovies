1cf45abb1262fef6aa88c3b59aa310195dd98e8b
newbi94 <newbi94@gmail.com>
2024-02-16 20:53:19 +0900

    24.02.16 part2
    isMovie만들어서
    movie & tv useQuery 합치기
    (queryFn은 말썽쟁이),
    Share.share,
    Platform.OS,
    생성 시간에 따른 data인식 문제,
    WebBrowser.openBrowserAsync,
    Linking.openURL,

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Share, Platform } from "react-native";
 import styled from "styled-components/native";
 import { Movie, TV, moviesApi, tvApi } from "../api";
 import Poster from "../components/Poster";
@@ -8,6 +8,10 @@ import { makeImgPath } from "../utils";
 import { BLACK_COLOR } from "../colors";
 import { LinearGradient } from "expo-linear-gradient";
 import { useQuery } from "@tanstack/react-query";
+import * as WebBrowser from "expo-web-browser";
+import { Ionicons } from "@expo/vector-icons";
+import Loader from "../components/Loader";
+import { TouchableOpacity } from "react-native";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -20,7 +24,6 @@ const Header = styled.View`
   justify-content: flex-end;
   padding: 0px 20px;
 `;
-
 const Background = styled.Image``;
 
 const Column = styled.View`
@@ -38,8 +41,21 @@ const Overview = styled.Text`
   color: ${(props) => props.theme.textColor};
   margin-top: 20px;
   padding: 0px 20px;
+  margin: 20px 0px;
 `;
-
+const Data = styled.View`
+  padding: 0px 20px;
+`;
+const VideoBtn = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+const BtnText = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-bottom: 10px;
+  line-height: 24px;
+  margin-left: 10px;
+`;
 type RootStackParamList = {
     Detail: Movie | TV;
 };
@@ -52,44 +68,86 @@ const Detail: React.FC<DetailScreenProps> = ({
     navigation: { setOptions },
     route: { params },
     }) => {
+    const isMovie = "original_title" in params;
 
         const {
-            isLoading: moviesLoading,
-            data: moviesData,
+            isLoading,
+            data,
         } = useQuery({
-            queryKey: ['movies', params.id],
-            queryFn: moviesApi.detail,
-            enabled: "original_title" in params,
+            queryKey: [isMovie ? 'movies':'tv', params.id],
+            queryFn: isMovie ? moviesApi.detail : tvApi.detail,
         });
-//영화에는 original_title에만 있고, tv show에는 original_name만 있는 것을 이용해
-//enabled를 설정했다. 영화를 클릭하면 tv fetch는 동작하지 않는다. 반대 경우는 영화 fetch가 동작하지 않는다.
-//자동완성을 통해 useQuery를 import할 경우 무심하게 엔터를 치면 tanstack이 아닌 일반 react-query로 import된다.
-//그렇게 되면 React Query v5인 지금, useQuery는 작동하지 않게된다.
-//params = nowplaying.results 라서 이것저것 데이터가 많긴하다.
-//하지만 그 이외에 비디오라던지 다른 사진들, params보다 넓은 범위에서 데이터를 끌어 쓰려면
-//fetch가 필요해서 이와 같이 fetching하는 것.
-//기존에 사용하던 overview, rate, title, poster 등은 이미 params로부터 받아내고 있음을 기억하자.
-
-        const {
-            isLoading: tvLoading,
-            data: tvData,
-        } = useQuery({
-            queryKey: ['tv',params.id],
-            queryFn: tvApi.detail,
-            enabled: "original_name" in params,
+        // queryFn의 No overload matchs this call은 정말 골치다..
+    
+    const shareMedia = async () => {
+      const isAndroid = Platform.OS === "android";
+     
+      const homepage = isMovie ?
+        `https://www.imdb.com/title/${data.imdb_id}/`
+        : data.homepage;
+      
+      if(isAndroid) {
+        await Share.share({
+          message:`${params.overview}\nCheck it out: ${homepage}`,
+          title:"original_title" in params
+          ? params.original_title
+          : params.original_name,
         });
+      } else {
+        await Share.share({
+          url: homepage,
+          title:"original_title" in params
+          ? params.original_title
+          : params.original_name,
+        });
+      };
+    };
+    
 
+    const ShareButton = () => (
+      <TouchableOpacity onPress={shareMedia}>
+        <Ionicons name="share-outline" color="white" size={24} />
+      </TouchableOpacity>
+    )
     useEffect(() => {
+      setOptions({
+        title: "original_title" in params ? "Movie" : "TV Show",
+      });
+    }, []);
+    useEffect(() => {
+      if (data) {
+        setOptions({
+          headerRight: () => <ShareButton />,
+        });
+      }
+    }, [data]);
+   /*  useEffect(() => {
         setOptions({
             title:
         "original_title" in params
           ? "Movie"
           : "TV Show",
+          headerRight: () => <ShareButton />
         });
-    }, []);
-    console.log("movie",moviesData)
-    console.log("tv", tvData)
-    
+    }, []); 
+    위와 같이 headerRight을 통해 Share버튼을 생성하는 코드를 짰지만,
+    만들어진 버튼 안에서는 data = undefined가 발생한다.
+    이해한 바로는 useQuery를 통해 data를 가져오는 fetching작업은 promise형태인데
+    그렇지 않은 headerRight을 통한 ShareButton 생성은 detail 컴포넌트로 넘어오면
+    즉시 이루어진다.
+    먼저 만들어진 버튼 안에서는 그 후 fetching을 통해 가져온 data를 인식하지 못하는 것(?).
+    그래서 useEffect를 분리해서 dependency를 data로 설정해 data를 가져오면
+    ShareButton을 생성해 data를 인식할 수 있게끔 한다.
+    */
+
+    const openYTLink = async (videoID: string) => {
+      const baseUrl = `https://m.youtube.com/watch?v=${videoID}`;
+      // await Linking.openURL(baseUrl);
+      await WebBrowser.openBrowserAsync(baseUrl);
+    };
+    //Linking을 사용하면 앱으로 나가지만 WebBrowser를 사용하면 이 앱 안에서
+    //url을 열기 때문에 앱을 벗어나지 않게 한다.
+
     return (
     <Container>
         <Header>
@@ -110,15 +168,18 @@ const Detail: React.FC<DetailScreenProps> = ({
                 </Title>
             </Column>
         </Header>
-        <Overview>
-            {params.overview}
-        </Overview>
+        <Data>
+        <Overview>{params.overview}</Overview>
+        {isLoading ? <Loader /> : null}
+        {data?.videos?.results?.map((video) => (
+          <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+            <Ionicons name="logo-youtube" color="white" size={24} />
+            <BtnText>{video.name}</BtnText>
+          </VideoBtn>
+        ))}
+      </Data>
     </Container>
-    )
-}
-//<LinearGradient>는 그라데이션 효과를 줄 수 있는 태그다.
-//expo install expo-linear-gradient 를 통해 설치하고 import해줘야 하는데,
-//자동으로 import가 되지 않아 수동으로 했다.
-//transparent는 검정색을 기본으로 하는 투명색이다(?).
+    )}//video의 출처를 YouTube로 제한한 상태.
+//다른 출처의 video만 있다면 아무것도 뜨지 않는다.
 
 export default Detail;
